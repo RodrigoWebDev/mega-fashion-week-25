d8e2e6b49143b11d294a88387c380d4fc35d1a5f
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2018-10-17 06:33:51 -0300

    Done almost things

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 $(function(){
+
+  //Navegação
   var li = $(".nav__menu li");
-  
+
   li.hover(function(){
     $(this).children().children().css('visibility', 'visible');
   }, function(){
     var img = $(this).children().children();
     $(this).children().children().css('visibility', 'hidden');
   })
+
+  //Timer
+  $("#countdown-timer")
+    .countdown("2018/10/20", function(event) {
+    $(this).html(
+      event.strftime('<div><span class="numero">%D</span><span class="dias">dias</span></div> <div><span class="numero">%H</span><span class="dias">hrs</span></div> <div><span class="numero">%M</span><span class="dias">mins</span></div> <div><span class="numero">%S</span><span class="dias">seg</span></div>')
+    );
+  });
 });
